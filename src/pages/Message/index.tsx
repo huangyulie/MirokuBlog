6389385c4e0daf561695fc42b00282e390227055
huangyulie <1279072972@qq.com>
2022-09-25 23:06:53 +0800

    1.15留言2.0

--- src/pages/Message/index.tsx
+++ src/pages/Message/index.tsx
@@ -1,12 +1,44 @@
-import { Card } from "antd";
-
-import React from "react";
+import { Card, Col, Input, Row } from "antd";
+import { MessageOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
 import "./index.less";
 
+const { TextArea } = Input;
+
 export default function Index() {
+  const [hidden, setHidden] = useState<boolean>(true);
+  const ClickHandle = () => {
+    setHidden(!hidden);
+  };
   return (
     <Card title={"留言"}>
-      <div>这是我的留言</div>
+      <div className="message-content">
+        <div className="message-img">
+          <img src="http://localhost:3001/imgs/react.webp" />
+        </div>
+        <div className="message-input">
+          <Row>
+            <Col xs={24} md={8} lg={8} xl={8} className="content-input">
+              <div className="input-item">
+                <Input></Input>
+              </div>
+            </Col>
+            <Col xs={24} md={8} lg={8} xl={8} className="content-input">
+              <div className="input-item">
+                <Input></Input>
+              </div>
+            </Col>
+            <Col xs={24} md={8} lg={8} xl={8} className="content-input">
+              <div className="input-item">
+                <Input></Input>
+              </div>
+            </Col>
+          </Row>
+          <div className="content-textarea">
+            <TextArea showCount maxLength={500} />
+          </div>
+        </div>
+      </div>
       <div className="message-main">
         <div className="left">
           <div className="img">
@@ -18,6 +50,10 @@ export default function Index() {
             <div className="name">Miroku</div>
             <div className="date">2012年达到</div>
             <div className="ip">来自dasd</div>
+            <div className="ico">
+              回复：
+              <MessageOutlined onClick={ClickHandle} />
+            </div>
           </div>
           <div className="content">
             哈哈,阿达 哈哈,阿达 哈哈,阿达 哈哈,阿达 哈哈,阿达 哈哈,阿达
@@ -42,6 +78,7 @@ export default function Index() {
             </div>
           </div>
         </div>
+        <div className={hidden ? "hidden" : "hidden block"}>132</div>
       </div>
     </Card>
   );
